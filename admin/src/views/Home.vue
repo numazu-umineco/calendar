92b2cdc7851593ba8a3085eb713b338c44a4759b
windyakin <windyakin@gmail.com>
2023-10-07 23:48:47 +0900

    Vuetify を導入

--- admin/src/views/Home.vue
+++ admin/src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <p>
+      <router-link to="/events/1">Event detail (id: 1) page link sample</router-link>
+    </p>
   </div>
 </template>
--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,6 +1,8 @@
+import path from 'path'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import Vuetify from '@vuetify/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
