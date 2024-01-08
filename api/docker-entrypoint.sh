ed561ca614516788764e0f5ea2d0ba0caff15eb1
windyakin <windyakin@gmail.com>
2024-01-08 16:43:11 +0900

    SecretManager からの mountPath はディレクトリしか指定できないっぽいので階層を分割する

--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -3,6 +3,6 @@
 if [ "${DISABLE_LITESTREAM}" != "" ] || [ "${RAILS_ENV}" = "test" ]; then
   exec "$@"
 else
-  litestream restore -if-replica-exists -if-db-not-exists -config /rails/litestream.yml /rails/db/litestream/database.sqlite3
-  exec litestream replicate -exec "$*" -config /rails/litestream.yml
+  litestream restore -if-replica-exists -if-db-not-exists -config /rails/litestream/config.yml /rails/db/litestream/database.sqlite3
+  exec litestream replicate -exec "$*" -config /rails/litestream/config.yml
 fi
