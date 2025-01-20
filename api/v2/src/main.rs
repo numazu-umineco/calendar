2f21ed91e660d039e9f792ccce0eaba99637905f
ug <gr8.distance@gmail.com>
2025-01-20 21:36:45 +0900

    actix-web: main config

--- api/v2/src/main.rs
+++ api/v2/src/main.rs
@@ -5,12 +5,14 @@ use crate::domain::entities::calendar_detail::CalendarDetail;
 use crate::domain::entities::calendar_event::CalendarEvent;
 use crate::infra::exporter::ics::IcsExporter;
 use crate::infra::repository::calendar::CalendarRepository;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use chrono::{DateTime, Utc};
 use infra::repository;
 use std::fs::File;
 use std::io::Write;
 
-fn main() {
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     let mut calendar = CalendarDetail::new(1, "沼津イベント".to_string());
 
     let event = CalendarEvent::new(
@@ -25,37 +27,11 @@ fn main() {
         false,
         None,
     );
-    let event1 = CalendarEvent::new(
-        1,
-        "野獣先輩".to_string(),
-        "お　ま　た　せ、アイスティーしかなかったんだけどいいかな？".to_string(),
-        "下北沢".to_string(),
-        Utc::now(),
-        Utc::now(),
-        35.1,
-        138.9,
-        false,
-        None,
-    );
-    let event2 = CalendarEvent::new(
-        1,
-        "tnok".to_string(),
-        "おいゴルァ! 降りろ! 概要書いてんのかコラ!".to_string(),
-        "下北沢".to_string(),
-        Utc::now(),
-        Utc::now(),
-        35.1,
-        138.9,
-        false,
-        None,
-    );
 
     calendar.add_event(event);
-    calendar.add_event(event1);
-    calendar.add_event(event2);
 
     let repo = CalendarRepository::new(calendar);
-    let ical = repo.to_ical();
-    let exporter = IcsExporter::new();
-    let _ = exporter.execute(ical, "calendar.ics");
+    let _ = repo.to_ical();
+
+    Ok(())
 }
