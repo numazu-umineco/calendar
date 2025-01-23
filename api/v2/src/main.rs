261b6730e573586becc0e06064f25a9d2cf16a1e
ug <gr8.distance@gmail.com>
2025-01-23 18:48:09 +0900

    admin APIへのアクセスをできるようにする

--- api/v2/src/main.rs
+++ api/v2/src/main.rs
@@ -2,12 +2,13 @@ mod domain;
 mod infra;
 mod use_cases;
 
-use crate::infra::adapters::public_routes::init;
+use crate::infra::adapters::admin_routes::init as admin_init;
+use crate::infra::adapters::public_routes::init as public_init;
 use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().configure(init))
+    HttpServer::new(|| App::new().configure(public_init).configure(admin_init))
         .bind("127.0.0.1:8080")?
         .run()
         .await
